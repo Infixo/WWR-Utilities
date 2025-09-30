726d80426373ba2f88b1f4b917b6007c2775948d
Infixo <swidwins@gmail.com>
2025-09-30 21:18:02 +0200

    Some performance improvements

--- Extensions.cs
+++ Extensions.cs
@@ -3,11 +3,8 @@
 using STM.Data;
 using STM.GameWorld;
 using STM.GameWorld.Users;
-using STM.UI;
-using STM.UI.Floating;
 using STMG.Engine;
 using STMG.UI.Control;
-using STVisual.Utility;
 
 namespace Utilities;
 
@@ -118,8 +115,7 @@ public static class WorldwideRushExtensions
             Dictionary<CityUser, int> passengers = city.GetAllPassengers();
             foreach (var destination in passengers)
             {
-                GrowArray<Line> tempLines = new();
-                if (city.IsConnectedTo(destination.Key, line, tempLines))
+                if (city.IsConnectedTo(destination.Key, line))
                 {
                     waiting += destination.Value;
                 }
@@ -129,54 +125,44 @@ public static class WorldwideRushExtensions
     }
 
 
-    public static bool IsConnectedTo(this CityUser from, CityUser direction, Line connection, GrowArray<Line> lines)//, GameScene scene)
-    {
-        bool isConnected = false;
-        GameScene scene = (GameScene)GameEngine.Last.Main_scene;
-        //ushort _company = scene.Session.Player;
-        //GrowArray<Line> lines = new GrowArray<Line>();
+    // Counters to count method calls
+    public static int CounterIsConn = 0; // counts IsConnectedTo
+    public static int CounterGetLine0 = 0;
+    public static int CounterGetLine1 = 0;
+    public static int CounterGetLine2 = 0;
+    public static int CounterGetLine3 = 0;
+    public static int CounterGetPath = 0; // counts GetPath calls
 
-        // Analyze existing direct connections
-        if (from.Connections_hash.Contains(direction.City))
+    /// <summary>
+    /// Checks if city <paramref name="from"/> is connected to city 
+    /// <paramref name="direction"/> via given line <paramref name="connection"/>.
+    /// </summary>
+    /// <param name="from">The starting city.</param>
+    /// <param name="direction">The target city.</param>
+    /// <param name="connection">The line used for connection.</param>
+    /// <returns>True if connected</returns>
+    public static bool IsConnectedTo(this CityUser from, CityUser direction, Line connection)
+    {
+        CounterIsConn++;
+        GameScene scene = (GameScene)GameEngine.Last.Main_scene;
+
+        // Check if this is city on the line itself
+        if (connection.Instructions.Contains(direction))
         {
-            // this looks for player's routes
-            /*
-            for (int i4 = 0; i4 < from.Routes.Count; i4++)
+            CounterGetLine0++;
+            return true;
+        }
+
+        // Analyze existing direct connections from any company
+        for (int i3 = 0; i3 < from.Routes.Count; i3++)
+        {
+            if (from.Routes[i3].Instructions.Contains(direction))
             {
-                if (from.Routes[i4].Vehicle.Company == _company && from.Routes[i4].Instructions.Contains(direction))
-                {
-                    Line _line6 = scene.Session.Companies[from.Routes[i4].Vehicle.Company].Line_manager.GetLine(from.Routes[i4].Vehicle);
-                    if (_line6 != null)
-                    {
-                        lines.AddSingle(_line6);
-                    }
-                }
-            }
-            */
-            //if (lines.Count > 0)
-            //{
-                //tooltip.AddBoldLabel(Localization.GetCity("direct_exists"), null, center: true, LabelPresets.Color_positive);
-            //}
-            //else
-            //{
-                //tooltip.AddBoldLabel(Localization.GetCity("direct_exists_other"), null, center: true, LabelPresets.Color_negative);
-            //}
-            // Find the connecting routes from any company
-            for (int i3 = 0; i3 < from.Routes.Count; i3++)
-            {
-                if (from.Routes[i3].Instructions.Contains(direction))
-                {
-                    Line _line7 = scene.Session.Companies[from.Routes[i3].Vehicle.Company].Line_manager.GetLine(from.Routes[i3].Vehicle);
-                    if (_line7 != null)
-                    {
-                        lines.AddSingle(_line7);
-                    }
-                    if (_line7 == connection)
-                        isConnected = true;
-                }
+                Line _line7 = scene.Session.Companies[from.Routes[i3].Vehicle.Company].Line_manager.GetLine(from.Routes[i3].Vehicle);
+                CounterGetLine1++;
+                if (_line7 == connection) return true;
             }
         }
-        if (isConnected) return true;
 
         // Analyze existing indirect connections
         for (int n = 0; n < from.Indirect.Count; n++)
@@ -192,116 +178,31 @@ public static class WorldwideRushExtensions
                     continue;
                 }
                 Line _line3 = scene.Session.Companies[from.Routes[l2].Vehicle.Company].Line_manager.GetLine(from.Routes[l2].Vehicle);
-                if (_line3 != null)
-                {
-                    lines.AddSingle(_line3);
-                    //if (from.Routes[l2].Vehicle.Company == _company)
-                    //{
-                    //_player = true;
-                    //}
-                }
-                if (_line3 == connection)
-                    isConnected = true;
+                CounterGetLine2++;
+                if (_line3 == connection) return true;
             }
         }
-        if (isConnected) return true;
 
-        // Search for indirect connections
-        //if (!isConnected)
-        //{
-        // searches for indirect connections from the player's lines - too narrow?
-        // could be mixed connections too as long as the connecting line is the one we are analyzing
-        //bool _player = false;
+        // Search for indirect connections from any company
         PathSearchData _data = PathSearch.GetData();
         _data.Open.Add(new PathSearchNode(from.City, 0, PathSearchNode.GetDistanceCost(from.City, direction.City), 0, 0));
-        CityPath _route = PathSearch.Get(from.City, direction.City, _data, int.MaxValue, scene);
+        CityPath _route = PathSearch.Get(from.City, direction.City, _data, int.MaxValue, scene, 0);
+        CounterGetPath++;
         if (_route.Path != null)
         {
-            for (int i2 = 0; i2 < from.Routes.Count; i2++)
-            {
-                if (!from.Routes[i2].Instructions.Contains(_route.Path[1].User))
+            //for (int m = 0; m < from.Routes.Count; m++)
+            //{
+                if (/*from.Routes[m]*/connection.Instructions.Contains(_route.Path[1].User))
                 {
-                    continue;
+                    //Line _line4 = scene.Session.Companies[from.Routes[m].Vehicle.Company].Line_manager.GetLine(from.Routes[m].Vehicle);
+                    //CounterGetLine3++;
+                    //if (_line4 == connection)
+                    return true;
                 }
-                Line _line5 = scene.Session.Companies[from.Routes[i2].Vehicle.Company].Line_manager.GetLine(from.Routes[i2].Vehicle);
-                if (_line5 != null)
-                {
-                    lines.AddSingle(_line5);
-                    //if (from.Routes[i2].Vehicle.Company == _company)
-                    //{
-                    //_player = true;
-                    //}
-                }
-                if (_line5 == connection)
-                    isConnected = true;
-            }
+            //}
         }
-        if (isConnected) return true;
-
-        //if (lines.Count > 0)
-        //{
-        //tooltip.AddBoldLabel(Localization.GetCity(_player ? "indirect_exists" : "indirect_exists_other"), null, center: true, _player ? LabelPresets.Color_positive : LabelPresets.Color_negative);
-        //}
-        //}
-        // is still not connected, then search for indirect connections from other companies
-        //if (!isConnected)
-        //{
-        _data = PathSearch.GetData();
-        _data.Open.Add(new PathSearchNode(from.City, 0, PathSearchNode.GetDistanceCost(from.City, direction.City), 0, 0));
-        _route = PathSearch.Get(from.City, direction.City, _data, int.MaxValue, scene, 0);
-        if (_route.Path != null)
-        {
-            for (int m = 0; m < from.Routes.Count; m++)
-            {
-                if (from.Routes[m].Instructions.Contains(_route.Path[1].User))
-                {
-                    Line _line4 = scene.Session.Companies[from.Routes[m].Vehicle.Company].Line_manager.GetLine(from.Routes[m].Vehicle);
-                    if (_line4 != null)
-                    {
-                        lines.AddSingle(_line4);
-                    }
-                    if (_line4 == connection)
-                        isConnected = true;
-                }
-            }
-        }
-        return isConnected;
+        return false;
     }
-    //if (lines.Count > 0)
-    //{
-    //tooltip.AddBoldLabel(Localization.GetCity("overcrowded_exsists"), null, center: true, LabelPresets.Color_negative);
-    //int _most = 1;
-    //for (int l = 2; l < _route.Path.Length - 1; l++)
-    //{
-    //if (_route.Path[l].User.Accepts_indirect > _route.Path[_most].User.Accepts_indirect)
-    //{
-    //_most = l;
-    //}
-    //}
-    /*
-    ButtonItem _locate = ButtonPresets.TextBlack(ContentRectangle.Zero, _route.Path[_most].User.GetNameWithIcon(scene), scene.Engine);
-    _locate.Control.horizontal_alignment = HorizontalAlignment.Center;
-    _locate.Control.Size_local = new Vector2(_locate.Label.Size_local.X + (float)MainData.Size_button, MainData.Size_button);
-    tooltip.AddContent(_locate.Control);
-    _locate.Control.OnButtonPress += (Action)delegate
-    {
-        if (Scene.tracking == _route.Path[_most].User)
-        {
-            _route.Path[_most].User.Select(Scene, track: true);
-        }
-        else
-        {
-            Scene.tracking = _route.Path[_most].User;
-        }
-    };
-    */
-    //}
-    //else
-    //{
-    //tooltip.AddBoldLabel(Localization.GetCity("no_valid_path"), null, center: true, LabelPresets.Color_negative);
-    //}
-    //}
-
 }
 
 #pragma warning restore CA1416 // Validate platform compatibility
