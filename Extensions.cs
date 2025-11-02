ca5ae14ab3cfac7550624246ef3446cfc8df1477
Infixo <swidwins@gmail.com>
2025-11-02 13:45:54 +0100

    Tree-walk method, unfortunately without distances does not work properly.

--- Extensions.cs
+++ Extensions.cs
@@ -6,6 +6,8 @@ using STM.Data.Entities;
 using STM.GameWorld;
 using STM.GameWorld.Users;
 using STMG.Engine;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace Utilities;
 
@@ -55,16 +57,18 @@ public static class WorldwideRushExtensions
 
 
     // Get all passengers waiting in the city, similar to CityUser.GetPassengers(CityUser destination) but returns all cities
-    public static Dictionary<CityUser, int> GetAllPassengers(this CityUser city)
+    public static void GetAllPassengers(this CityUser city, Dictionary<CityUser, int> travellers)
     {
-        Dictionary<CityUser, int> travellers = [];
+        //Dictionary<CityUser, int> travellers = [];
 
         // helper
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         void RegisterTravellers(CityUser cityUser, int people)
         {
-            if (!travellers.ContainsKey(cityUser))
-                travellers[cityUser] = 0;
-            travellers[cityUser] += people;
+            //if (!travellers.ContainsKey(cityUser))
+                //travellers[cityUser] = 0;
+            if (!travellers.TryAdd(cityUser, people))
+                travellers[cityUser] += people;
         }
 
         // review destinations
@@ -79,7 +83,7 @@ public static class WorldwideRushExtensions
         for (int k = 0; k < city.Indirect.Count; k++)
         {
             Passengers dest = city.Indirect.Items[k];
-            if (dest.People != 0)
+            if (dest.People > 0)
                 RegisterTravellers(dest.Destination.User, dest.People);
         }
 
@@ -87,11 +91,11 @@ public static class WorldwideRushExtensions
         for (int j = 0; j < city.Returns.Count; j++)
         {
             ReturnDestination dest = city.Returns[j];
-            if (dest.Ready != 0)
+            if (dest.Ready > 0)
                 RegisterTravellers(dest.Home.User, dest.Ready);
         }
 
-        return travellers;
+        //return travellers;
     }
 
     /// <summary>
@@ -171,48 +175,64 @@ public static class WorldwideRushExtensions
         CounterIsConn++;
         GameScene scene = (GameScene)GameEngine.Last.Main_scene;
 
+        // TEST TEST
+        return from.ConnectsTo(direction);
+
         // Check if this is city on the line itself
-        if (connection.Instructions.Contains(direction))
+        if (connection.Instructions.Contains(from) && connection.Instructions.Contains(direction))
         {
             CounterGetLine0++;
             return true;
         }
 
         // Analyze existing direct connections from any company
+        // 2025-11-01 Performance analysis - GetLine is super inefficient
+        // However, here we can reverse the search - since we only need to check a specific line!
+        // If one RouteInstance from that line connects the cities then ALL do!
+        /*
         for (int i3 = 0; i3 < from.Routes.Count; i3++)
         {
             if (from.Routes[i3].Instructions.Contains(direction))
             {
-                Line _line7 = scene.Session.Companies[from.Routes[i3].Vehicle.Company].Line_manager.GetLine(from.Routes[i3].Vehicle);
+                Line _line7 = scene.Session.Companies[from.Routes[i3].Vehicle.Company].Line_manager.GetLine(from.Routes[i3].Vehicle); // [3622]
                 CounterGetLine1++;
                 if (_line7 == connection) return true;
             }
         }
+        */
 
         // Analyze existing indirect connections
         for (int n = 0; n < from.Indirect.Count; n++)
         {
-            if (from.Indirect[n].Destination != direction.City)
+            if (from.Indirect[n].Destination == direction.City && connection.Instructions.Contains(from.Indirect[n].Next.User))
             {
-                continue;
+                CounterGetLine2++;
+                return true;
             }
+            /*
             for (int l2 = 0; l2 < from.Routes.Count; l2++)
             {
-                if (!from.Routes[l2].Instructions.Contains(from.Indirect[n].Next.User))
+                if (!from.Routes[l2].Instructions.Contains(from.Indirect[n].Next.User)) // [5064]
                 {
                     continue;
                 }
-                Line _line3 = scene.Session.Companies[from.Routes[l2].Vehicle.Company].Line_manager.GetLine(from.Routes[l2].Vehicle);
+                // Similar trick as above - use reverse check!
+                Line _line3 = scene.Session.Companies[from.Routes[l2].Vehicle.Company].Line_manager.GetLine(from.Routes[l2].Vehicle); // [15100]
                 CounterGetLine2++;
                 if (_line3 == connection) return true;
             }
+            */
         }
+
+        // 2025-11-01 Limit expensive searches
+        if (!from.ConnectsTo(direction))
+            return false;
 
         // Search for indirect connections from any company
         PathSearchData _data = PathSearch.GetData();
         // 2025-10-25 Patch 1.1.13 adjusted_start_cost added
         _data.Open.Add(new PathSearchNode(from.City, 0, 0, PathSearchNode.GetDistanceCost(from.City, direction.City), 0, 0));
-        CityPath _route = PathSearch.Get(from.City, direction.City, _data, int.MaxValue, scene, 0);
+        CityPath _route = PathSearch.Get(from.City, direction.City, _data, int.MaxValue, scene, 0); // [76200]
         CounterGetPath++;
         if (_route.Path != null)
         {
@@ -221,12 +241,52 @@ public static class WorldwideRushExtensions
             if (/*from.Routes[m]*/connection.Instructions.Contains(_route.Path[1].User))
             {
                 //Line _line4 = scene.Session.Companies[from.Routes[m].Vehicle.Company].Line_manager.GetLine(from.Routes[m].Vehicle);
-                //CounterGetLine3++;
+                CounterGetLine3++;
                 //if (_line4 == connection)
                 return true;
             }
             //}
         }
+        return false;
+    }
+
+
+    public static bool ConnectsTo(this CityUser from, CityUser to)
+    {
+        // (1) Direct connection
+        if (from.Connections_hash.Contains(to.City))
+        {
+            CounterGetLine0++;
+            return true;
+        }
+
+        // (2) One-stop connection
+        for (int i = 0; i < from.Connections.Count; i++)
+            if (from.Connections[i].User.Connections_hash.Contains(to.City))
+            {
+                CounterGetLine1++;
+                return true;
+            }
+
+        // (3) Two-stop connection
+        for (int i = 0; i < from.Connections.Count; i++)
+            for (int j = 0; j < from.Connections[i].User.Connections.Count; j++)
+                if (from.Connections[i].User.Connections[j].User.Connections_hash.Contains(to.City))
+                {
+                    CounterGetLine2++;
+                    return true;
+                }
+
+        // (4) Three-stop connection
+        for (int i = 0; i < from.Connections.Count; i++)
+            for (int j = 0; j < from.Connections[i].User.Connections.Count; j++)
+                for (int k = 0; k < from.Connections[i].User.Connections[j].User.Connections.Count; k++)
+                    if (from.Connections[i].User.Connections[j].User.Connections[k].User.Connections_hash.Contains(to.City))
+                    {
+                        CounterGetLine3++;
+                        return true;
+                    }
+
         return false;
     }
 
@@ -290,23 +350,135 @@ public static class WorldwideRushExtensions
 // Line extensions
 public static class Line_Extensions
 {
+    public struct CityInfo
+    {
+        public int Level = 0;
+        public bool Access = true;
+        public CityInfo (int n, bool a) { Level = n; Access = a; }
+        public override readonly string ToString() => $"{Level}-{Access}";
+    };
+
+    /// <summary>
+    /// Adds to the set all cities directly connected with them
+    /// </summary>
+    /// <param name="cities"></param>
+    private static void ExpandConnections(HashSet<City> cities, HashSet<City> processed)
+    {
+        HashSet<City> next = [];
+        foreach (City city in cities.Except(processed))
+        {
+            next.UnionWith(city.User.Connections_hash);
+            processed.Add(city);
+        }
+        cities.UnionWith(next); // Add to the original set
+    }
+
+
+    // 2025-11-01 New approach
+    /// <summary>
+    /// Returns number of passengers waiting to use the line in the set of cities.
+    /// </summary>
+    /// <param name="line"></param>
+    /// <param name="cities"></param>
+    /// <returns></returns>
+    public static long GetWaiting(this Line line, City[] cities)
+    {
+        // Build a list of passengers - make sure we only process cities on the line
+        Dictionary<CityUser, int> passengers = [];
+        foreach(City city in cities.Where(c => line.Instructions.Contains(c.User)))
+            city.User.GetAllPassengers(passengers);
+
+        if (passengers.Count == 0) return 0; // safety
+
+        // Build connections hash-set
+        HashSet<City> connected = [.. line.Instructions.Cities.Select(c => c.City)];
+        HashSet<City> processed = [];
+        ExpandConnections(connected, processed); // this will add all cities connected ditectly to the line-cities
+        ExpandConnections(connected, processed); // 1st hop
+        ExpandConnections(connected, processed); // 2nd hop
+
+        // Calculate waiting passengers
+        long waiting = 0;
+        foreach (var dest in passengers)
+            if (connected.Contains(dest.Key.City))
+                waiting += dest.Value;
+
+        return waiting;
+    }
+
+
     public static long GetWaiting(this Line line, CityUser city)
     {
-        long waiting = 0;
-        Dictionary<CityUser, int> passengers = city.GetAllPassengers();
-        foreach (var destination in passengers)
+        if (!line.Instructions.Contains(city)) return 0;
+
+        // Build list of cities accessible via the line
+        Dictionary<CityUser, CityInfo> network = [];
+        network.Add(city, new CityInfo(0, false));
+        foreach(CityUser c in line.Instructions.Cities)
+            if (c != city) network.Add(c, new CityInfo(1, true));
+        HashSet<Route> processed = [line.Instructions];
+
+        // Determine cities that can be accessed via the line
+        void ExpandConnections(int level)
         {
-            if (city.IsConnectedTo(destination.Key, line))
-            {
-                waiting += destination.Value;
-            }
+            Dictionary<CityUser, CityInfo> next = [];
+            foreach (var pair in network.Where(x => x.Value.Level == level))
+                for (int i = 0; i < pair.Key.Routes.Count; i++)
+                {
+                    Route route = pair.Key.Routes[i].Instructions;
+                    if (!processed.Contains(route))
+                    {
+                        foreach (CityUser c in route.Cities)
+                            next.TryAdd(c, new CityInfo(level+1, true));
+                        processed.Add(route);
+                    }
+                }
+            foreach (var pair in next) network.TryAdd(pair.Key, pair.Value);
         }
+        ExpandConnections(1);
+        ExpandConnections(2);
+        ExpandConnections(3);
+
+        // Remove cities that can be accessed more directly
+        processed = [line.Instructions]; // reset
+        void RemoveConnections(int level)
+        {
+            foreach (var pair in network.Where(x => x.Value.Level == level-1 && !x.Value.Access))
+                for (int i = 0; i < pair.Key.Routes.Count; i++)
+                {
+                    Route route = pair.Key.Routes[i].Instructions;
+                    if (!processed.Contains(route))
+                    {
+                        foreach (CityUser c in route.Cities)
+                        {
+                            if (network.ContainsKey(c) && level < network[c].Level) // this is better connection
+                                    network[c] = new CityInfo(level, false);
+                                // same level or worse - all other cities from here will be same or worse
+                        }
+                        processed.Add(route);
+                    }
+                } 
+        }
+        RemoveConnections(1);
+        RemoveConnections(2);
+        RemoveConnections(3);
+
+        // Process passengers
+        long waiting = 0;
+        Dictionary<CityUser, int> passengers = [];
+        city.GetAllPassengers(passengers);
+        HashSet<CityUser> filtered = [.. network.Where(x => x.Value.Access).Select(x => x.Key)];
+        foreach (var destination in passengers)
+            if (filtered.Contains(destination.Key))
+                waiting += destination.Value;
+        
         return waiting;
     }
 
 
     public static long GetWaiting(this Line line)
     {
+        //return line.GetWaiting([.. line.Instructions.Cities.Select(c => c.City)]);
         long waiting = 0;
         foreach (CityUser city in line.Instructions.Cities)
             waiting += line.GetWaiting(city);
